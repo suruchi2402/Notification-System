e6636742f8b49ede65f8210eb4693b09ec1e94d8
suruchi2402 <pinkp7840@gmail.com>
2026-06-29 01:24:26 +0530

    Business Logics

--- src/main/java/com/notification/notification_system/service/NotificationService.java
+++ src/main/java/com/notification/notification_system/service/NotificationService.java
@@ -3,10 +3,12 @@ package com.notification.notification_system.service;
 import com.notification.notification_system.dto.NotificationRequest;
 import com.notification.notification_system.entity.Notification;
 import com.notification.notification_system.enums.NotificationStatus;
+import com.notification.notification_system.exception.NotificationNotFoundException;
 import com.notification.notification_system.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
 @Service
 public class NotificationService {
@@ -29,5 +31,29 @@ public class NotificationService {
         notification.setCreatedAt(now);
         notification.setUpdatedAt(now);
         return repository.save(notification);
+    }
+
+    public List<Notification> getALlNotification(){
+        return repository.findAll();
+    }
+
+    public Notification getNotificationBYId(Long id){
+//        Optional<Notification> optionalNotification=repository.findById(id);
+        return repository.findById(id).orElseThrow(()-> new NotificationNotFoundException(id));
+    }
+
+    public Notification updateNotificationBYId(Long id, NotificationRequest request){
+        Notification notification = getNotificationBYId(id);
+        notification.setRecipient(request.getRecipient());
+        notification.setSubject(request.getSubject());
+        notification.setMessage(request.getMessage());
+        notification.setChannel(request.getChannel());
+        notification.setUpdatedAt(LocalDateTime.now());
+        return repository.save(notification);
+    }
+
+    public void deleteNotificationById(Long id){
+        Notification notification = getNotificationBYId(id);
+        repository.delete(notification);
     }
 }
