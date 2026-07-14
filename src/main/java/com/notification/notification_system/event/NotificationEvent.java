b3637e89bf19151388bc699c082c124f51bbdf5b
suruchi2402 <pinkp7840@gmail.com>
2026-07-15 00:43:03 +0530

    NotificationEvent

---
+++ src/main/java/com/notification/notification_system/event/NotificationEvent.java
@@ -0,0 +1,56 @@
+package com.notification.notification_system.event;
+
+import com.notification.notification_system.enums.NotificationChannel;
+
+public class NotificationEvent {
+
+    private Long id;
+
+    private String recipient;
+
+    private String subject;
+
+    private String message;
+
+    private NotificationChannel channel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public NotificationChannel getChannel() {
+        return channel;
+    }
+
+    public void setChannel(NotificationChannel channel) {
+        this.channel = channel;
+    }
+}
