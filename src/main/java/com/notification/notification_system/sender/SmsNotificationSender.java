d7f1b7528a9b688034e71ec4bfe9ae43f419b107
suruchi2402 <pinkp7840@gmail.com>
2026-07-15 00:45:48 +0530

    dead retry check

--- src/main/java/com/notification/notification_system/sender/SmsNotificationSender.java
+++ src/main/java/com/notification/notification_system/sender/SmsNotificationSender.java
@@ -2,6 +2,7 @@ package com.notification.notification_system.sender;
 
 import com.notification.notification_system.entity.Notification;
 import com.notification.notification_system.enums.NotificationChannel;
+import com.notification.notification_system.event.NotificationEvent;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,7 @@ public class SmsNotificationSender implements NotificationSender{
         return NotificationChannel.PUSH;
     }
     @Override
-    public void send(Notification notification){
-        System.out.println("Sending SMS Notification to "+ notification.getRecipient());
+    public void send(NotificationEvent event){
+        System.out.println("Sending SMS Notification to "+ event.getRecipient());
     }
 }
